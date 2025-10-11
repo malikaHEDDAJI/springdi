eef4b501e7c18fe24190bdfc5404c44ea0322de2
MalikaHEDDAJI03 <Malika.Heddaji@emsi-edu.ma>
2025-10-11 09:34:24 +0100

    Initial commit tp2

---
+++ src/main/java/com/example/dao/DaoImpl2.java
@@ -0,0 +1,23 @@
+package com.example.dao;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+/*
+@Component("dao2")
+public class DaoImpl2 implements IDao {
+    @Override
+    public double getValue() {
+        return 150.0;
+    }
+}
+
+ */
+@Component("dao2")
+@Profile("dev")
+public class DaoImpl2 implements IDao {
+    @Override
+    public double getValue() {
+        System.out.println("Version de développement");
+        return 150.0;
+    }
+}
---
+++ src/main/java/com/example/presentation/Presentation2.java
@@ -0,0 +1,36 @@
+package com.example.presentation;
+
+import com.example.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+/*
+@Configuration
+@ComponentScan(basePackages = {"com.example.dao", "com.example.metier"})
+public class Presentation2 {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Presentation2.class);
+
+        IMetier metier = context.getBean(IMetier.class);
+
+        System.out.println("Résultat = " + metier.calcul());
+    }
+}
+
+ */
+
+@Configuration
+@ComponentScan(basePackages = {"com.example.dao", "com.example.metier"})
+public class Presentation2 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.getEnvironment().setActiveProfiles("dev");
+        context.register(Presentation2.class);
+        context.refresh();
+
+        IMetier metier = context.getBean(IMetier.class);
+
+        System.out.println("Résultat = " + metier.calcul());
+    }
+}
